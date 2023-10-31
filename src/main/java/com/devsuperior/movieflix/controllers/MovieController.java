533fe80b40f2dc367e0368ff445bcf726bb1c58b
blicuadriDev <blicuadriDev@gmail.com>
2023-10-31 14:38:57 -0300

    List movies by genre id

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -2,12 +2,14 @@ package com.devsuperior.movieflix.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.MovieCardDTO;
@@ -22,8 +24,17 @@ public class MovieController {
 	
     @PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
 	@GetMapping
-	public ResponseEntity<Page<MovieCardDTO>> getMovies(Pageable pageable) {
-		Page<MovieCardDTO> dto = service.pagedMovies(pageable);
+	public ResponseEntity<Page<MovieCardDTO>> getMovies(
+			@RequestParam (value="genreId", defaultValue="0") Long genreId, 
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam (value = "size", defaultValue = "4") Integer size,
+			@RequestParam (value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam (value = "orderBy", defaultValue = "title") String orderBy) {
+    	
+		
+    	PageRequest pageRequest = PageRequest.of(page, size,  Direction.valueOf(direction), orderBy);
+		
+		Page<MovieCardDTO> dto = service.pagedMovies(genreId, pageRequest);
 		return ResponseEntity.ok().body(dto);
 	}
     
@@ -34,4 +45,7 @@ public class MovieController {
     	MovieCardDTO dto = service.getMovie(id);
 		return ResponseEntity.ok().body(dto);
 	}
+    
+    
+    
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -4,12 +4,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
@@ -19,17 +22,37 @@ public class MovieService {
 	@Autowired
 	private MovieRepository repository;
 	
+	@Autowired
+	private GenreRepository genreRepository;
+	
 	@Transactional(readOnly = true)
-	public Page<MovieCardDTO> pagedMovies(Pageable pageable){
-		Page<Movie> entity = repository.findAll(pageable);
-		return entity.map( x -> new MovieCardDTO(x));
+	public Page<MovieCardDTO> pagedMovies(Long genreId, PageRequest pageRequest){
+		
+		if(genreId == 0 ) {
+			Page<Movie> entity = repository.findAll(pageRequest);
+			return entity.map( x -> new MovieCardDTO(x));
+		}
+		else {
+			Genre genre = genreRepository.getReferenceById(genreId);
+			Page<Movie> entity = repository.findByGenreId(genre, pageRequest);
+			return entity.map( x -> new MovieCardDTO(x));
+		}
+		
 	}
+	
+	
 
 	@Transactional(readOnly = true)
 	public MovieCardDTO getMovie(Long id) {
 		Optional<Movie> aux = repository.findById(id);
 		Movie entity = aux.orElseThrow(() -> new ResourceNotFoundException("Movie not found!"));
 		return new MovieCardDTO(entity);
+	}
+
+	public Page<MovieCardDTO> getMovieByGenre(Long genreId, Pageable pageable) {
+		
+
+		return null;
 	}
 	
 }
